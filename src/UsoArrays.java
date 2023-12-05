829e3c12b7a39a9f80f4a71bb93ddbd25bd80dfe
daniinigogarcia98 <daniinigogarcia98@gmail.com>
2023-12-05 21:02:49 +0100

    Inicio Proyecto

---
+++ src/UsoArrays.java
@@ -0,0 +1,14 @@
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		Arrays miArray=new Arrays(5);
+		
+		miArray.getDatosArray();
+
+	}
+
+}
+
